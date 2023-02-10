f3c025470d521eae795839620f5c4f9f4dc4aad1
kendydung <levu26122000@gmail.com>
2023-02-10 10:11:56 +0700

    add new project

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-11-20 05:59:05 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-11-20T05:59:05+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2017-11-27T05:36:45+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ templates/protostar/css/readme.txt
@@ -0,0 +1,3 @@
+Since Joomla 3.5.0, the Protostar template loads custom CSS code from the file at /templates/protostar/css/user.css, if the file exists.
+
+Renaming your custom.css to user.css should fix the issue.
